550e8b2ee23c0a583d657eee426a3c3f5fe53351
Pascal Greilich <pascal.greilich@gmail.com>
2019-03-02 12:12:25 -0500

    wip

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -12,12 +12,6 @@ import 'firebase/firestore';
 // REACT_APP_MESSAGING_SENDER_ID=
 
 const config = {
-  // apiKey: "AIzaSyBQs6yJ2euReTS-aTua1KDKGdm0bLeCRO0",
-  // authDomain: "firecalculator-6b0f6.firebaseapp.com",
-  // databaseURL: "https://firecalculator-6b0f6.firebaseio.com",
-  // messagingSenderId: "816782235195",
-  // projectId: "YOUR firecalculator-6b0f6",
-  // storageBucket: "firecalculator-6b0f6.appspot.com"
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
