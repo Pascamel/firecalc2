079993e45616b0c063627a257ee9f4eef78f402b
Pascal Greilich <pascal.greilich@gmail.com>
2019-05-18 17:08:23 -0400

    wip

--- src/pages/Charts/yearlyChart.tsx
+++ src/pages/Charts/yearlyChart.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { connect } from 'react-redux';
 import { Badge, Button, ButtonGroup, Col, ListGroup, ListGroupItem, Row } from 'reactstrap';
@@ -31,6 +32,14 @@ class YearlyChart extends React.Component<IProps, IState> {
     this.setState({year: year});
   }
 
+  prevYear = () => {
+    this.setState({year: this.state.year - 1});
+  }
+
+  nextYear = () => {
+    this.setState({year: this.state.year + 1});
+  }
+
   percentageYear = (year: string) => {
     const { bank } = this.props;
     const month = (parseInt(year) === (new Date().getFullYear())) ? (new Date().getMonth() + 1) : 12;
@@ -39,7 +48,7 @@ class YearlyChart extends React.Component<IProps, IState> {
   }
   
   render() {
-    const { data, mobile, chart } = this.props;
+    const { bank, data, mobile, chart } = this.props;
     const { year } = this.state;
     
     return (
@@ -49,19 +58,22 @@ class YearlyChart extends React.Component<IProps, IState> {
             {Object.entries(data).map((y) => ( 
               <ListGroupItem key={y[0]} className="text-left" color={parseInt(y[0]) === year ? 'secondary' : 'link'} tag={Button} onClick={() => this.clickDate(parseInt(y[0]))}>
                 {y[0]}
-                <Badge color="success" pill className="pull-right mt-1">
+                <Badge color={this.percentageYear(y[0]) > 0 ? 'success' : 'danger'} pill className="pull-right mt-1">
                   {helpers.percentage(this.percentageYear(y[0]), 0, true)}
                 </Badge>
               </ListGroupItem>
             ))} 
           </ListGroup>}
-          {mobile && <ButtonGroup >
-            <Button>
-              Left
-              <Badge pill>14</Badge>
+          {mobile && <ButtonGroup style={{width: '100%'}} color="light" className="mb-3">
+            <Button color="outline-secondary" onClick={this.prevYear} disabled={year === bank.headers.firstYear}>
+              <FontAwesomeIcon icon="backward" />
             </Button>
-            <Button>Middle</Button>
-            <Button>Right</Button>
+            <Button color="outline-secondary" disabled={true} block>
+              {year}
+            </Button>
+            <Button color="outline-secondary" onClick={this.nextYear} disabled={year === (new Date()).getFullYear()}>
+              <FontAwesomeIcon icon="forward" />
+            </Button>
           </ButtonGroup>}
         </Col>
         <Col md={10} sm={12}>
