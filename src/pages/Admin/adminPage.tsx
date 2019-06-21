cf58158e7bbb97bd791bec3e303ccd8de2200ef0
Pascal <pascal@reonomy.com>
2019-06-21 17:47:40 -0400

    function component

--- src/pages/Admin/adminPage.tsx
+++ src/pages/Admin/adminPage.tsx
@@ -1,55 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Alert, Col, Container, Row } from 'reactstrap';
 
 import { HeaderPanel, LoadingPanel } from '../../components';
 import { firestore } from '../../firebase';
 import ListUsers from './listUsers';
 
-interface IState {
-  loading: boolean;
-  users: any;
+type UserType = {
+  id: string,
+  email?: string,
+  type?: number
 }
-export default class AdminPageBase extends React.Component<{}, IState> {
-  constructor(props: any) {
-    super(props);
 
-    this.state = {
-      loading: true,
-      users: []
-    }
-  }
+export default function AdminPageBase () {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState<Array<UserType>>([]);
 
-  async componentDidMount() {
-    const users = await firestore.getUsers();
-    const list: any = [];
-    
-    users.docs.forEach(doc => list.push({id: doc.id, ...doc.data()}));
+  useEffect(() => {
+    firestore.getUsers().then(res => {
+      const list: Array<UserType> = [];
+      
+      res.docs.forEach(doc => list.push({id: doc.id, ...doc.data()}));
 
-    this.setState({users: list, loading: false});
-  }
+      setLoading(false);
+      setUsers(list);
+    });
+  }, []);
 
-  render() {
-    const { users, loading } = this.state;
-    return (
-      <>
-        {loading && <LoadingPanel />}
-        {!loading && <HeaderPanel title="Admin" />}
-        {!loading && <Container fluid className="top-shadow">
-          <Row>
-            <Col className="pl-0 pr-0">
-              <Container>
-                <Row>
-                  <Col>
-                    <Alert color="background">
-                      <ListUsers users={users} />
-                    </Alert>
-                  </Col>
-                </Row>
-              </Container>
-            </Col>
-          </Row>
-        </Container>}
-      </>
-    );
-  }
+  return (
+    <>
+      {loading && <LoadingPanel />}
+      {!loading && <HeaderPanel title="Admin" />}
+      {!loading && <Container fluid className="top-shadow">
+        <Row>
+          <Col className="pl-0 pr-0">
+            <Container>
+              <Row>
+                <Col>
+                  <Alert color="background">
+                    <ListUsers users={users} />
+                  </Alert>
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>}
+    </>
+  );
 }
--- src/pages/Admin/listUsers.tsx
+++ src/pages/Admin/listUsers.tsx
@@ -2,40 +2,42 @@ import _ from 'lodash';
 import React from 'react';
 import * as ROLES from '../../constants/roles';
 
-interface IProps {
-  users: any
+type UserType = {
+  id: string,
+  email?: string,
+  type?: number
 }
 
-export default class ListUsers extends React.Component<IProps, {}> {
-  render() {
-    const { users } = this.props;
-    
-    const labelType = (type: number) => {
-      return _(ROLES).invert().get(type, 'none');
-    }
+interface IProps {
+  users: Array<UserType>
+}
 
-    return (
-      <>
-        <b>{users.length}</b> users
-        <table className="table table-striped mt-3">
-          <thead>
-            <tr>
-              <th style={{width: '30%'}}>ID</th>
-              <th style={{width: '60%'}}>Email</th>
-              <th style={{width: '10%'}}>Type</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user: any) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.email}</td>
-                <td>{labelType(user.type)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </>      
-    );
+export default function ListUsers({ users }: IProps) {
+  const labelType = (type: number) => {
+    return _(ROLES).invert().get(type, 'none');
   }
+
+  return (
+    <>
+      <b>{users.length}</b> users
+      <table className="table table-striped mt-3">
+        <thead>
+          <tr>
+            <th style={{width: '30%'}}>ID</th>
+            <th style={{width: '60%'}}>Email</th>
+            <th style={{width: '10%'}}>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user: any) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.email}</td>
+              <td>{labelType(user.type)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>      
+  );
 }
