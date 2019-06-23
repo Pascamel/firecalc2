f022183366297fb94e04ad32304da38e867082e1
Pascal Greilich <pascal.greilich@gmail.com>
2019-06-23 09:50:05 -0400

    piggy bank

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import './styles/fire.scss';
+import './styles/tables.scss';
+import './styles/404.scss';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import store from './store';
 import { App } from './components/App';
-
-import 'bootstrap/dist/css/bootstrap.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
-import './styles/fire.scss';
-import './styles/flame.scss';
-import './styles/tables.scss';
-import './styles/404.scss';
-
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
--- src/pages/Month/finances.tsx
+++ src/pages/Month/finances.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Alert, Col } from 'reactstrap';
 
-import Bank, { ISavingsHeader } from '../../bank';
+import Bank from '../../bank';
 import { StaticAmount } from '../../components';
 import { AppState } from '../../store';
 import MonthIncome from './monthIncome';
