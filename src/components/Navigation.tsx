e6295f1c579030520dd662900c1c8dabbb25efdc
Pascal Greilich <pascal.greilich@gmail.com>
2019-06-23 12:32:27 -0400

    wip

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import { LocationState } from 'history';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link, NavLink, withRouter } from 'react-router-dom';
 import {
@@ -17,7 +18,6 @@ import {
   UncontrolledDropdown
 } from 'reactstrap';
 import { compose } from 'recompose';
-import { LocationState } from 'history';
 
 import * as CHARTS from '../constants/charts';
 import * as ROUTES from '../constants/routes';
@@ -25,184 +25,158 @@ import helpers from '../helpers';
 import { AppState } from '../store';
 import { SignOutLink } from './SignOutLink';
 
-
 interface IProps {
-  location: LocationState;
-  authUser: firebase.User|null;
+  location?: LocationState;
+  authUser?: firebase.User | null;
 }
 
-interface IState {
-  isOpen: boolean;
-  authUser: firebase.User|null;
-}
+// interface IState {
+//   isOpen: boolean;
+//   authUser: firebase.User | null;
+// }
 
-class NavigationAuth extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false,
-      authUser: null //needs cleaning
-    };
+function NavigationAuth(props: IProps) {
+  const { location, authUser } = props;
+  const [isOpen, setIsOpen] = useState(false);
+  const DEFAULT_CHART = ROUTES.CHARTS.replace(':type', CHARTS.URL.INCOME_VS_SAVINGS);
+  
+  const toggle = () => {
+    setIsOpen(!isOpen);
   }
 
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
+  const toggleIfOpen = () => {
+    if (isOpen) toggle();
   }
 
-  toggleIfOpen() {
-    if (this.state.isOpen) this.toggle();
-  }
-
-  navLinkClass = (route: string) => {
+  const navLinkClass = (route: string) => {
     const classNames = ['nav-link']
-    if (route.split('/')[1] === this.props.location.pathname.split('/')[1]) classNames.push('active');
+    if (route.split('/')[1] === location.pathname.split('/')[1]) classNames.push('active');
 
     return classNames.join(' ');
-  }
+  }  
 
-  render() {
-    const { authUser } = this.props;
-    const DEFAULT_CHART = ROUTES.CHARTS.replace(':type', CHARTS.URL.INCOME_VS_SAVINGS);
-
-    return (
-      <Navbar light expand="md">
-        <NavLink className="navbar-brand" to={ROUTES.DASHBOARD}>
-          FireCalc
-        </NavLink>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink className={this.navLinkClass(ROUTES.MONTH)} to={helpers.currentMonthRoute()} onClick={()=>this.toggleIfOpen()}>
-                <FontAwesomeIcon icon={['far', 'calendar-alt']} className="mr-1" />
-                Month
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to={ROUTES.REVENUES} onClick={()=>this.toggleIfOpen()}>
-                <FontAwesomeIcon icon="user-tie" className="mr-1" />
-                Revenues
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to={ROUTES.SAVINGS} onClick={()=>this.toggleIfOpen()}>
-                <FontAwesomeIcon icon="piggy-bank" className="mr-1" />
-                Savings
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className={this.navLinkClass(ROUTES.CHARTS)} to={DEFAULT_CHART} onClick={()=>this.toggleIfOpen()}>
-                <FontAwesomeIcon icon="chart-area" className="mr-1" />
-                Charts
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to={ROUTES.SETTINGS} onClick={()=>this.toggleIfOpen()}>
-                <FontAwesomeIcon icon="cogs" className="mr-1" />
-                Settings
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to={ROUTES.ADMIN} onClick={()=>this.toggleIfOpen()}>
-                <FontAwesomeIcon icon="solar-panel" className="mr-1" />
-                Admin
-              </NavLink>
-            </NavItem>
-          </Nav> 
-          <Nav className="ml-auto" navbar>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>{authUser ? authUser.email : 'User'}</DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem >
-                  <Link className="no-link" to={ROUTES.ACCOUNT} onClick={()=>this.toggleIfOpen()}>
-                    Account
-                  </Link>
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  <SignOutLink />
-                </DropdownItem>
-              </DropdownMenu> 
-            </UncontrolledDropdown> 
-          </Nav>
-        </Collapse>
-      </Navbar>
-    );
-  }
+  return (
+    <Navbar light expand="md">
+      <NavLink className="navbar-brand" to={ROUTES.DASHBOARD}>
+        FireCalc
+      </NavLink>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav navbar>
+          <NavItem>
+            <NavLink className={navLinkClass(ROUTES.MONTH)} to={helpers.currentMonthRoute()} onClick={toggleIfOpen}>
+              <FontAwesomeIcon icon={['far', 'calendar-alt']} className="mr-1" />
+              Month
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to={ROUTES.REVENUES} onClick={toggleIfOpen}>
+              <FontAwesomeIcon icon="user-tie" className="mr-1" />
+              Revenues
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to={ROUTES.SAVINGS} onClick={toggleIfOpen}>
+              <FontAwesomeIcon icon="piggy-bank" className="mr-1" />
+              Savings
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className={navLinkClass(ROUTES.CHARTS)} to={DEFAULT_CHART} onClick={toggleIfOpen}>
+              <FontAwesomeIcon icon="chart-area" className="mr-1" />
+              Charts
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to={ROUTES.SETTINGS} onClick={toggleIfOpen}>
+              <FontAwesomeIcon icon="cogs" className="mr-1" />
+              Settings
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to={ROUTES.ADMIN} onClick={toggleIfOpen}>
+              <FontAwesomeIcon icon="solar-panel" className="mr-1" />
+              Admin
+            </NavLink>
+          </NavItem>
+        </Nav> 
+        <Nav className="ml-auto" navbar>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>{authUser ? authUser.email : 'User'}</DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem >
+                <Link className="no-link" to={ROUTES.ACCOUNT} onClick={toggleIfOpen}>
+                  Account
+                </Link>
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+                <SignOutLink />
+              </DropdownItem>
+            </DropdownMenu> 
+          </UncontrolledDropdown> 
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
 }
 
-class NavigationNonAuth extends React.Component<{}, IState> {
-  constructor(props: IProps) {
-    super(props);
+function NavigationNonAuth(props: IProps) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false,
-      authUser: null
-    };
+  const toggle = () => {
+    setIsOpen(!isOpen);
   }
 
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
+  const toggleIfOpen = () => {
+    if (isOpen) toggle();
   }
 
-  toggleIfOpen() {
-    if (this.state.isOpen) this.toggle();
-  }
-
-  render() {
-    return (
-      <Navbar light expand="md">
-        <NavLink className="navbar-brand" to={ROUTES.DASHBOARD}>
-          FireCalc
-        </NavLink>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink className="nav-link" to={ROUTES.SIGN_IN} onClick={()=>this.toggleIfOpen()}>
-                Sign In
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to={ROUTES.SIGN_UP} onClick={()=>this.toggleIfOpen()}>
-                Sign Up
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    );
-  }
+  return (
+    <Navbar light expand="md">
+      <NavLink className="navbar-brand" to={ROUTES.DASHBOARD}>
+        FireCalc
+      </NavLink>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink className="nav-link" to={ROUTES.SIGN_IN} onClick={toggleIfOpen}>
+              Sign In
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to={ROUTES.SIGN_UP} onClick={toggleIfOpen}>
+              Sign Up
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
 }
 
-class NavigationBase extends React.Component<any, IState> {
-  render() {
-    const { location, authUser } = this.props;
+function NavigationBase(props: any) {
+  const { location, authUser } = props;
+
+  console.log('props', props);
     
-    return (
-      <Container fluid className="nav-container">
-        <Row>
-          <Col>
-            <Container>
-              <Row>
-                <Col>
-                  {authUser ? <NavigationAuth location={location} authUser={authUser} /> : 
-                  <NavigationNonAuth />}
-                </Col>
-              </Row>
-            </Container>
-          </Col>
-        </Row>
-      </Container> 
-    );
-  }
+  return (
+    <Container fluid className="nav-container">
+      <Row>
+        <Col>
+          <Container>
+            <Row>
+              <Col>
+                {authUser ? <NavigationAuth location={location} authUser={authUser} /> : 
+                <NavigationNonAuth location={location} authUser={authUser} />}
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    </Container> 
+  );
 }
 
 const mapStateToProps = (state: AppState) => {
