7a655faa0bd5dbae128b516c1bf0029a11953eb8
Pascal <pascal@reonomy.com>
2019-06-30 09:43:45 -0400

    wip

--- src/pages/Admin/listUsers.tsx
+++ src/pages/Admin/listUsers.tsx
@@ -1,46 +1,32 @@
-import _ from 'lodash';
 import React from 'react';
 
-import * as ROLES from '../../constants/roles';
+import { IUser } from './interfaces';
+import User from './user';
 
 interface IProps {
-  users: Array<{
-    id: string,
-    email?: string,
-    type?: number
-  }>;
+  users: Array<IUser>;
 }
 
 const ListUsers = (props: IProps) => {
   const { users } = props;
   
-  const labelType = (type: number) => {
-    return _(ROLES).invert().get(type, 'none');
-  }
-
   return (
     <>
       <b>{users.length}</b> users
-      <table className="table table-striped mt-3">
+      <table className="table mt-3">
         <thead>
           <tr>
+            <th style={{width: '2%'}}></th>
             <th style={{width: '30%'}}>ID</th>
-            <th style={{width: '60%'}}>Email</th>
+            <th style={{width: '58%'}}>Email</th>
             <th style={{width: '10%'}}>Type</th>
           </tr>
         </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.email}</td>
-              <td>{user.type ? labelType(user.type) : 'N/A'}</td>
-            </tr>
-          ))}
-        </tbody>
+        {users.map(user => <User key={user.id} user={user} />)}
       </table>
     </>      
   );
 }
+
 
 export default ListUsers;
