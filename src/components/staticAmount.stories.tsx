b852d31548f895582d1900b8abcad1d9cda11b02
Pascal Greilich <pascal.greilich@gmail.com>
2020-01-06 20:53:13 -0500

    experiment with storybook

---
+++ src/components/staticAmount.stories.tsx
@@ -0,0 +1,29 @@
+import { storiesOf } from "@storybook/react";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import rootReducers from '../reducers';
+import { StaticAmount } from '.'; ///components';
+
+let store = createStore(rootReducers);
+
+storiesOf('StaticAMount', module)
+  .add('Default',
+      () => {
+        return (
+          <Provider store={store}>
+            <StaticAmount>{123.45}</StaticAmount>
+          </Provider>
+        )
+      }
+  )
+  .add('No decimals',
+    () => {
+      return (
+        <Provider store={store}>
+          <StaticAmount hide-decimals>{123.45}</StaticAmount>
+        </Provider>
+      );
+    }
+  );
