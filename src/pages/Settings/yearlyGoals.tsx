1f53df020d06ea0c80d0c5347d1203f4ec81567d
Pascal Greilich <pascal.greilich@gmail.com>
2020-01-12 09:21:40 -0500

    y goals

--- src/pages/Settings/yearlyGoals.tsx
+++ src/pages/Settings/yearlyGoals.tsx
@@ -5,6 +5,7 @@ import { Alert, Col, Row } from 'reactstrap';
 import { updateValue } from '../../actions';
 import Bank from '../../bank';
 import FireAmount from '../../components/fireAmount';
+import helpers from '../../helpers';
 import { AppState } from '../../store';
 
 interface IProps {
@@ -13,6 +14,29 @@ interface IProps {
   onUpdateValue: (index: string, indexes: string[], amount: number|boolean) => void;
 };
 
+interface IYoyLabel {
+  bank: Bank.IBank;
+  year: number;
+}
+
+const YoyLabel: React.FunctionComponent<IYoyLabel> = (props) => {
+  const { year, bank } = props;
+  const currentYear = new Date().getFullYear();
+  const defaultLabel = <span>-</span>;
+  
+  if (year <= bank.headers.firstYear) return defaultLabel;
+  if (year > currentYear + 1) return defaultLabel;
+  if (!(bank.savingsYearHeaders.goals[year-1] > 0)) return defaultLabel;
+  if (!(bank.savingsYearHeaders.goals[year] > 0)) return defaultLabel;
+
+  const value = bank.savingsYearHeaders.goals[year] - bank.savingsYearHeaders.goals[year-1]
+  
+  return (
+    <span className={value >= 0 ? 'text-success':'text-danger'}>
+      ${helpers.amount(value, false, true)} 
+    </span>
+  );
+}
 
 const YearlyGoals = (props: IProps) => {
   const { bank, bankLoaded } = props;
@@ -28,29 +52,26 @@ const YearlyGoals = (props: IProps) => {
         </Col>
       </Row>
       <Row>
-        <Col>
-          <table className="table table-borderless table-sm table-equal-width">
-            <thead>
-              <tr>
-                {[...Array(currentYear - bank.headers.firstYear + 2)].map((_,i) => i + bank.headers.firstYear).map(v => (
-                  <th key={v}>
-                    {v}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                {[...Array(currentYear - bank.headers.firstYear + 2)].map((_,i) => i + bank.headers.firstYear).map(year => (
-                  <td key={year}>
-                    <FireAmount extraClassName="bold" callback-props={['savingsYearHeaders', 'goals', year]} />
-                  </td>
-                ))}
-              </tr>
-            </tbody>
-          </table>
+        <Col sm={4} xs={12}>
+          Year
+        </Col>
+        <Col sm={{size: 2, offset: 2}} xs={12}>
+          YoY
         </Col>
       </Row>
+      {[...Array(currentYear - bank.headers.firstYear + 2)].map((_,i) => i + bank.headers.firstYear).map(year => (
+        <Row key={year}>
+          <Col sm={4} xs={8}>
+            <span className="label-fake-input smaller mb-1">{year}</span>
+            <div className="pull-right">
+              <FireAmount extraClassName="label-fake-input" callback-props={['savingsYearHeaders', 'goals', year]} />
+            </div>
+          </Col>
+          <Col sm={{size: 4, offset: 2}} xs={4}>
+            <YoyLabel bank={bank} year={year} />
+          </Col>
+        </Row>
+      ))}
     </Alert>
   );
 }
@@ -69,6 +90,7 @@ const mapDispatchToProps = (dispatch: Dispatch<any>) => {
     }
   };
 }
+
 export default connect(
   mapStateToProps,
   mapDispatchToProps
