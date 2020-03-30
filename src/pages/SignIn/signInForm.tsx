5265b6d43b90f821f0e181edc689dd72ae6ed475
Pascal Greilich <pascal.greilich@gmail.com>
2020-03-29 21:48:54 -0400

    oops

--- src/pages/SignIn/signInForm.tsx
+++ src/pages/SignIn/signInForm.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { RouteComponentProps } from 'react-router';
-import {
-    Alert, Button, Form, FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText
-} from 'reactstrap';
-
-import { Icon } from '../../components';
 import * as ROUTES from '../../constants/routes';
 import { auth } from '../../firebase';
+import { Alert, Button, Form, FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { RouteComponentProps } from 'react-router';
+
 
 export const SignInForm = (props: RouteComponentProps) => {
-  const [error, setError] = useState<Error | null>(null);
+  const [error, setError] = useState<Error|null>(null);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -22,17 +20,14 @@ export const SignInForm = (props: RouteComponentProps) => {
   }, [error]);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    auth
-      .doSignInWithEmailAndPassword(email, password)
-      .then(() => {
-        setError(null);
-        setEmail('');
-        setPassword('');
-        props.history.push(ROUTES.HOME);
-      })
-      .catch(error => {
-        setError(error);
-      });
+    auth.doSignInWithEmailAndPassword(email, password).then(() => {
+      setError(null);
+      setEmail('');
+      setPassword('');
+      props.history.push(ROUTES.HOME);
+    }).catch(error => {
+      setError(error);
+    });
 
     event.preventDefault();
   };
@@ -46,39 +41,24 @@ export const SignInForm = (props: RouteComponentProps) => {
           <InputGroup className="mt-2">
             <InputGroupAddon addonType="prepend">
               <InputGroupText>
-                <Icon icon={['far', 'user']} />
+                <FontAwesomeIcon icon={['far', 'user']} />
               </InputGroupText>
             </InputGroupAddon>
-            <Input
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              type="text"
-              placeholder="Email Address"
-            />
+            <Input value={email} onChange={e => setEmail(e.target.value)} type="text" placeholder="Email Address" />
           </InputGroup>
         </FormGroup>
         <FormGroup>
           <InputGroup>
             <InputGroupAddon addonType="prepend">
               <InputGroupText>
-                <Icon icon="unlock-alt" />
+                <FontAwesomeIcon icon="unlock-alt" />
               </InputGroupText>
             </InputGroupAddon>
-            <Input
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              type="password"
-              placeholder="Password"
-            />
+            <Input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
           </InputGroup>
         </FormGroup>
         <FormGroup>
-          <Button
-            color="primary"
-            block={true}
-            disabled={isInvalid}
-            type="submit"
-          >
+          <Button color="primary" block={true} disabled={isInvalid} type="submit">
             Sign In
           </Button>
         </FormGroup>
@@ -87,4 +67,4 @@ export const SignInForm = (props: RouteComponentProps) => {
       {error && <Alert color="danger">{error.message}</Alert>}
     </>
   );
-};
+}
