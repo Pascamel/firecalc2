e628201e36f7a893ae426c5a72e594d5c735f21d
Pascal Greilich <pascal.greilich@gmail.com>
2020-05-25 11:09:03 -0400

    conflicts

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -3,18 +3,11 @@ import {
     faCalendarAlt, faCheckSquare, faSave, faSquare, faUser
 } from '@fortawesome/free-regular-svg-icons';
 import {
-<<<<<<< HEAD
-    faBackward, faChartArea, faCheck, faChevronDown, faChevronRight, faChevronUp, faClipboardList,
-    faCogs, faColumns, faCreditCard, faEdit, faExclamationTriangle, faEye, faEyeSlash, faForward,
-    faInfo, faLaptopCode, faLightbulb, faPen, faPiggyBank, faSignInAlt, faSolarPanel, faSpinner,
-    faTimes, faTrashAlt, faUniversity, faUnlockAlt, faUserPlus, faUserTie
-=======
     faBackward, faChartArea, faCheck, faChevronDown, faChevronRight, faChevronUp, faColumns,
     faCreditCard, faEdit, faExclamationTriangle, faEye, faEyeSlash, faForward, faHistory, faInfo,
     faLaptopCode, faLightbulb, faPen, faPiggyBank, faSignInAlt, faSlidersH, faSolarPanel, faSpinner,
     faStepBackward, faStepForward, faSync, faTimes, faTrashAlt, faUniversity, faUnlockAlt,
     faUserPlus, faUserTie
->>>>>>> 5b462ae67b8fb08357e9177ec30de810c1beea93
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
@@ -26,11 +19,6 @@ library.add(
   faChevronDown,
   faChevronRight,
   faChevronUp,
-<<<<<<< HEAD
-  faClipboardList,
-  faCogs,
-=======
->>>>>>> 5b462ae67b8fb08357e9177ec30de810c1beea93
   faColumns,
   faCreditCard,
   faEdit,
